f435ad5efeca61479de2de3c689910d8fc999cf1
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2026-02-12 18:26:13 +0100

    feat: Navigation added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {  
+        path: 'navigation', 
+        loadComponent: () => import('./navigation/navigation.component').then(m => m.NavigationComponent) }
+];
