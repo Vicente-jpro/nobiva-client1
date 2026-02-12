eea1b60713ac7e550c977c51c1ca30225d1fede5
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2026-02-12 23:11:11 +0100

    feat: Index updated.

--- src/app/house/house-tree/house-tree.ts
+++ src/app/house/house-tree/house-tree.ts
@@ -1,37 +1,46 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MatTreeModule} from '@angular/material/tree';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { ChangeDetectionStrategy, Component, Output } from '@angular/core';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { EventEmitter } from '@angular/core';
 
 /**
  * Food data with nested structure.
  * Each node has a name and an optional list of children.
  */
 interface FoodNode {
-  name: string;
-  children?: FoodNode[];
+    name: string;
+    children?: FoodNode[];
 }
 
 @Component({
-  selector: 'app-house-tree',
-  imports: [MatTreeModule, MatButtonModule, MatIconModule],
-  templateUrl: './house-tree.html',
-  styleUrl: './house-tree.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-house-tree',
+    imports: [MatTreeModule, MatButtonModule, MatIconModule],
+    templateUrl: './house-tree.html',
+    styleUrl: './house-tree.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HouseTree {
 
-  dataSource = EXAMPLE_DATA;
+    dataSource = EXAMPLE_DATA;
 
-  childrenAccessor = (node: FoodNode) => node.children ?? [];
+    @Output() selectedHouseTreeEvent = new EventEmitter<string>();
 
-  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+    childrenAccessor = (node: FoodNode) => node.children ?? [];
+
+    hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+
+    sendSelectedTreeToHouse(name: string): void {
+        this.selectedHouseTreeEvent.emit(name);
+        console.log('Selected tree node:', name);
+    }
+
 }
 
 const EXAMPLE_DATA: FoodNode[] = [
-  {
-    name: 'Casas',
-    children: [{name: 'Nova'}, {name: 'Listar'}],
-  },
+    {
+        name: 'Casas',
+        children: [{ name: 'Nova' }, { name: 'Listar' }],
+    },
 
 ];
