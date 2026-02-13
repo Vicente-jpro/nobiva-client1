ddfedd0f427fb3887fc8fea5e342787250662336
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2026-02-13 18:02:33 +0100

    feat: Progress bar added.

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,21 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatChipsModule} from '@angular/material/chips';
+import { Footer } from "./footer/footer";
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
-   imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatChipsModule, RouterOutlet],
+   imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatMenuModule, 
+    MatChipsModule, 
+    RouterOutlet, 
+    Footer,
+    MatProgressBarModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
