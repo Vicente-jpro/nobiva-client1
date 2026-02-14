7b47ffb5edcde9a284cff42e6474420b827a33ec
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2026-02-14 20:04:23 +0100

    feat: New fiels added.

--- src/app/user/signup/signup.ts
+++ src/app/user/signup/signup.ts
@@ -5,6 +5,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatAnchor } from "@angular/material/button";
 import { RouterLink } from "@angular/router";
 
+
+interface User{   
+    username: string;
+    email: string;
+    password: string;    
+    passwordConfirmed: string;
+    roles: string[];
+}
+
 @Component({
   selector: 'app-signup',
   imports: [
@@ -17,5 +26,31 @@ import { RouterLink } from "@angular/router";
   styleUrl: './signup.scss',
 })
 export class Signup {
+  private formBuilder = inject(FormBuilder);
+
+    user: User = {
+    email: '',
+    username: '',
+    password: '',
+    passwordConfirmed: '',
+    roles: []
+  }
+
+  loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      passwordConfirmed: ['', [Validators.required, Validators.minLength(3)]],
+      roles: ['', [Validators.required]]
+    });
+
+  onSubmit(){
+    
+    this.user.email = this.loginForm.value.email ?? '';
+    this.user.password = this.loginForm.value.password ?? '';
+    
+    console.log(this.loginForm.value);
+    // { firstName: 'Nancy', lastName: 'Drew' }
+  }
 
 }
