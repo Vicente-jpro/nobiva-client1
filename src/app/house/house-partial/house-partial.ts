504fcb6c18c84d0e21fe62b1991fd2c6e6a86ca4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2026-03-19 18:56:08 +0100

    feat: update house partial component to use optimized image loading; remove unused NgOptimizedImage import

--- src/app/house/house-partial/house-partial.ts
+++ src/app/house/house-partial/house-partial.ts
@@ -1,5 +1,4 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
@@ -7,7 +6,7 @@ import { HouseResponse } from '../../models/house/house-response';
 
 @Component({
   selector: 'app-house-partial',
-  imports: [NgOptimizedImage, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './house-partial.html',
   styleUrl: './house-partial.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
